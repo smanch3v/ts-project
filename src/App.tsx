8ce0096573210b7f00813ad8c2e101ef65414fd9
smanch3v <svetoslavmanchev@gmail.com>
2021-12-14 11:35:20 +0100

    added model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
   return (
     <div>
-
+      <Todos items={todos} />
     </div>
   );
 }
