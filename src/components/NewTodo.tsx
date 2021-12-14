fbd03215d451dbbee3f01f01f316538ab8a9555f
smanch3v <svetoslavmanchev@gmail.com>
2021-12-14 13:24:24 +0100

    forgot to save

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,11 @@
+import React from "react";
+
 const NewTodo = () => {
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" />
       <button>Add Todo</button>
